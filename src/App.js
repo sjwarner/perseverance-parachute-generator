4c4736e530f0ec36973deafe9ecda29119b265e0
Sam Warner <sam_warner@talktalk.net>
2021-02-23 12:24:37 +0000

    Use created parachute svg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import PerseveranceParachute from "./perseverance-parachute";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <PerseveranceParachute />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Encode your own parachute!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/steltzner/status/1364076615932645379"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Inspired by the Perseverance Parachute code crackers
         </a>
       </header>
     </div>
