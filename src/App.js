afc65ae86e6a2da772731b948099f801e78e6b06
Sam Warner <sam_warner@talktalk.net>
2021-02-23 12:04:23 +0000

    Add labels and placeholders for inputs

--- src/App.js
+++ src/App.js
@@ -23,25 +23,33 @@ function App() {
       </div>
       <div className="flex flex-col Input-pane">
           <div className="tw-input-field">
+            <label htmlFor="outer-ring">Outer Ring</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal mb-4"
               type="text"
-              placeholder="Outer ring"
+              id="outer-ring"
+              placeholder="N 118 10 31 W 34 11 58"
             />
+            <label htmlFor="outer-ring">2nd Ring</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal mb-4"
               type="text"
-              placeholder="2nd Ring"
+              id="2nd-ring"
+              placeholder="THINGS"
             />
+            <label htmlFor="outer-ring">3rd Ring</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal mb-4"
               type="text"
-              placeholder="3rd Ring"
+              id="3rd-ring"
+              placeholder="MIGHTY"
             />
+            <label htmlFor="outer-ring">Inner Ring</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal mb-4"
               type="text"
-              placeholder="Inner ring"
+              id="inner-ring"
+              placeholder="DARE"
             />
           </div>
       </div>
