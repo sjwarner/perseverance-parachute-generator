9f6d73f80e70a43f694f5efa2080041604ff2e34
Sam Warner <sam_warner@talktalk.net>
2021-02-23 13:05:56 +0000

    Make inner ring work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import PerseveranceParachute from "./perseverance-parachute";
 
-function App() {
+function encodeToPosition(text) {
+    const encodedArray = [...text].map(a => parseInt(a, 36) - 9).filter(a => a >= 0);
+    const paddingArray = Array(8 - encodedArray.length).fill(0);
+
+    const array = encodedArray.concat(paddingArray);
+
+    return array.map(num => (num >>> 0).toString(2).padStart(8, "0"))
+}
+
+const EMPTY_ARRAY = ["00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000"]
+
+const App = () => {
+  const [innerRing, setInnerRing] = useState("");
+  const [secondRing, setSecondRing] = useState("");
+  const [thirdRing, setThirdRing] = useState("");
+  const [outerRing, setOuterRing] = useState("");
+
+  const [encodedInnerRing, setEncodedInnerRing] = useState(EMPTY_ARRAY);
+  const [encodedSecondRing, setEncodedSecondRing] = useState(EMPTY_ARRAY);
+  const [encodedThirdRing, setEncodedThirdRing] = useState(EMPTY_ARRAY);
+  const [encodedOuterRing, setEncodedOuterRing] = useState(EMPTY_ARRAY);
+
+  useEffect(() => {
+    setEncodedInnerRing(encodeToPosition(innerRing))
+  }, [innerRing])
+
+  useEffect(() => {
+    setEncodedSecondRing(encodeToPosition(secondRing))
+  }, [secondRing])
+
+  useEffect(() => {
+    setEncodedThirdRing(encodeToPosition(thirdRing))
+  }, [thirdRing])
+
+  useEffect(() => {
+    setEncodedOuterRing(encodeToPosition(outerRing))
+  }, [outerRing])
+
   return (
     <div className="App flex flex-row">
       <div className="flex flex-col Parachute-pane">
-        <PerseveranceParachute />
+        <PerseveranceParachute
+          encodedInnerRing={encodedInnerRing}
+          encodedSecondRing={encodedSecondRing}
+          encodedThirdRing={encodedThirdRing}
+          encodedOuterRing={encodedOuterRing}
+        />
           <p className="mt-4">
             Encode your own parachute!
           </p>
@@ -29,6 +72,8 @@ function App() {
               type="text"
               id="outer-ring"
               placeholder="N 118 10 31 W 34 11 58"
+              maxLength={8}
+              onChange={e => setOuterRing(e.target.value)}
             />
             <label htmlFor="outer-ring">2nd Ring</label>
             <input
@@ -36,6 +81,8 @@ function App() {
               type="text"
               id="2nd-ring"
               placeholder="THINGS"
+              maxLength={8}
+              onChange={e => setSecondRing(e.target.value)}
             />
             <label htmlFor="outer-ring">3rd Ring</label>
             <input
@@ -43,6 +90,8 @@ function App() {
               type="text"
               id="3rd-ring"
               placeholder="MIGHTY"
+              maxLength={8}
+              onChange={e => setThirdRing(e.target.value)}
             />
             <label htmlFor="outer-ring">Inner Ring</label>
             <input
@@ -50,6 +99,8 @@ function App() {
               type="text"
               id="inner-ring"
               placeholder="DARE"
+              maxLength={8}
+              onChange={e => setInnerRing(e.target.value)}
             />
           </div>
       </div>
